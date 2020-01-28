76786a54487f96df4d4c933c34a1a55fc4b100f6
fenrice <49913941+fenrice@users.noreply.github.com>
2020-01-28 03:47:39 +0200

    Create exploratory_data_analysis.py

---
+++ exploratory_data_analysis.py
@@ -0,0 +1,15 @@
+'''
+Checking the version of which Spark and Python installed is important as it changes very quickly and drastically.
+Reading the wrong documentation can cause lots of lost time and unnecessary frustration!
+
+This course touches on a lot of concepts you may have forgotten, so if you ever need a quick refresher, 
+download the PySpark Cheat Sheet and keep it handy!
+'''
+
+
+# Return spark version
+print(spark.version)
+
+# Return python version
+import sys
+print(sys.version_info)
